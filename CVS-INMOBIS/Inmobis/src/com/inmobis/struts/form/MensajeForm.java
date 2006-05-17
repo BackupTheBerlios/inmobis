ff79dd638edaedf736d169790700693ade521c47
joseluis <joseluis>
2006-05-17 07:54:10 +0000

    *** empty log message ***

--- CVS-INMOBIS/Inmobis/src/com/inmobis/struts/form/MensajeForm.java
+++ CVS-INMOBIS/Inmobis/src/com/inmobis/struts/form/MensajeForm.java
@@ -16,7 +16,15 @@ import org.apache.struts.action.ActionMapping;
  * XDoclet definition:
  * @struts.form name="MensajeForm"
  */
+@SuppressWarnings("serial")
 public class MensajeForm extends ActionForm {
+	protected String idMensaje;
+	protected String asunto;
+	protected String texto;
+	protected String origen;
+	protected String destino;
+	protected Character leido;  //vale s/n
+	protected String fecha;
 
 	// --------------------------------------------------------- Instance Variables
 
@@ -44,6 +52,62 @@ public class MensajeForm extends ActionForm {
 	public void reset(ActionMapping mapping, HttpServletRequest request) {
 
 		// TODO Auto-generated method stub
+	}
+
+	public String getAsunto() {
+		return asunto;
+	}
+
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+
+	public String getDestino() {
+		return destino;
+	}
+
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getIdMensaje() {
+		return idMensaje;
+	}
+
+	public void setIdMensaje(String idMensaje) {
+		this.idMensaje = idMensaje;
+	}
+
+	public Character getLeido() {
+		return leido;
+	}
+
+	public void setLeido(Character leido) {
+		this.leido = leido;
+	}
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
 	}
 
 }
