7901264d39387cbcfe009b5a3134992d703b13f5
itzipg <itzipg>
2006-05-19 13:27:36 +0000

    *** empty log message ***

--- CVS-INMOBIS/Inmobis/src/com/inmobis/struts/form/EditaEmpleadoPrevForm.java
+++ CVS-INMOBIS/Inmobis/src/com/inmobis/struts/form/EditaEmpleadoPrevForm.java
@@ -1,6 +1,3 @@
-//Created by MyEclipse Struts
-// XSL source (default): platform:/plugin/com.genuitec.eclipse.cross.easystruts.eclipse_4.0.0/xslt/JavaClass.xsl
-
 package com.inmobis.struts.form;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,37 +8,90 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionMessage;
 
+public class EditaEmpleadoPrevForm extends ActionForm{
+	private static final Logger i_log = Logger.getLogger(EditaClientePrevForm.class);
+	private String idUsuario;
+	//datos personales
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private String fechaNacimiento;
+	private String tipoEmpleado;
+	private String porcentaje;
+	private String dni;
 
-public class EditaEmpleadoPrevForm extends ActionForm {
-
-	private static final Logger i_log = Logger.getLogger(RegistraEmpleadoForm.class);
-	
-	public void reset(ActionMapping mapping, HttpServletRequest request) {
-		
+	public void reset(ActionMapping mapping, HttpServletRequest request){
+		idUsuario="";
+		nombre="";
+		apellido1="";
+		apellido2="";
+		dni="";
+		tipoEmpleado="";
+		porcentaje="";
+		fechaNacimiento="";
 	}
 	
-	public ActionErrors validate(ActionMapping mapping,
-								HttpServletRequest request) {
+	public ActionErrors validate(ActionMapping mapping, HttpServletRequest request)
+	{
 		ActionErrors errors = new ActionErrors();
-
+			//TODO ver que hay solo 1 seleccionado
 		if (i_log.isInfoEnabled()){
-			i_log.info("Antes de comprobar los errores. Nº errores: "+errors.size());
+			i_log.info("Datos obtenidos del Ver: "+idUsuario+" "+nombre+" "+apellido1+" "+apellido2+" "+dni+" "+fechaNacimiento+" "+tipoEmpleado);
 		}
-		//Tendre que comprobar que solo hay 1 empleado seleccionado (la lista que me llega tiene longitud 1?) 
-		
-		if (i_log.isInfoEnabled()){
-			i_log.info("Despues de comprobar los errores. Nº errores: "+errors.size());
-		}
-		
 		return errors;
 	}
-	
+
 	//Getters
-	
+	public String getIdUsuario(){
+		return this.idUsuario;
+	}
+	public String getNombre(){
+		return this.nombre;
+	}
+	public String getApellido1(){
+		return this.apellido1;
+	}
+	public String getApellido2(){
+		return this.apellido2;
+	}
+	public String getDni(){
+		return this.dni;
+	}
+	public String getFechaNacimiento(){
+		return this.fechaNacimiento;
+	}
+	public String getTipoEmpleado(){
+		return this.tipoEmpleado;
+	}
+	public String porcentaje(){
+		return this.porcentaje;
+	}
 	
 	//Setters
-	
-
-
+	public void setIdUsuario(String id){
+		this.idUsuario=id.trim();
+	}
+	public void setNombre(String nombre){
+		this.nombre=nombre.trim();
+	}
+	public void setApellido1(String ap1){
+		this.apellido1=ap1.trim();
+	}
+	public void setApellido2(String ap2){
+		this.apellido2=ap2.trim();
+	}
+	public void setDni(String dni){
+		this.dni=dni.trim();
+	}
+	public void setFechaNacimiento(String fecha){
+		this.fechaNacimiento=fecha.trim();
+	}
+	public void setTipoEmpleado(String tipo){
+		this.tipoEmpleado=tipo.trim();
+	}
+	public void setPorcentaje(String p){
+		this.porcentaje=p.trim();
+	}
 }
+
 
--- CVS-INMOBIS/Inmobis/src/com/inmobis/struts/form/EditaPisoPrevForm.java
+++ CVS-INMOBIS/Inmobis/src/com/inmobis/struts/form/EditaPisoPrevForm.java
@@ -1,13 +1,96 @@
 package com.inmobis.struts.form;
 
-public class EditaPisoPrevForm {
+import javax.servlet.http.HttpServletRequest;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
 
+public class EditaPisoPrevForm extends ActionForm{
+	private static final Logger i_log = Logger.getLogger(EditaClientePrevForm.class);
+	private String idInmueble;
+	//Datos inmueble
+    private String numHab;
+    private String tipo;
+    private String zona;
+	private String metros;
+    private String regimen;
+    private String precio;
+    private String datosDeInteres;
+
+	public void reset(ActionMapping mapping, HttpServletRequest request){
+		this.idInmueble="";
+    	this.numHab="";
+    	this.tipo="";
+    	this.zona="";
+    	this.metros="";
+	    this.regimen="";
+	    this.precio="";
+	    this.datosDeInteres="";	
+	}
+	
+	public ActionErrors validate(ActionMapping mapping, HttpServletRequest request)
+	{
+		ActionErrors errors = new ActionErrors();
+			//TODO ver que hay solo 1 seleccionado
+		if (i_log.isInfoEnabled()){
+			i_log.info("Datos obtenidos del Ver: "+idInmueble+" "+numHab+" "+tipo+" "+zona+" "+metros+" "+precio+" "+datosDeInteres);
+		}
+		return errors;
 	}
 
+	//Getters
+	//getters
+	public String getIdInmueble(){
+		return idInmueble;
+	}
+	public String getNumHab() {
+		return numHab;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public String getZona() {
+		return zona;
+	}
+	public String getDatosDeInteres() {
+		return datosDeInteres;
+	}
+	public String getMetros() {
+		return metros;
+	}
+	public String getPrecio() {
+		return precio;
+	}
+	public String getRegimen() {
+		return regimen;
+	}
+	//Setters
+	public void setIdInmueble(String id){
+		this.idInmueble=id.trim();
+	}
+	public void setNumHab(String numHab) {
+		this.numHab = numHab.trim();
+	}	
+	public void setTipo(String tipo) {
+		this.tipo = tipo.trim();
+	}	
+	public void setZona(String zona) {
+		this.zona = zona.trim();
+	}
+	public void setDatosDeInteres(String datosDeInteres) {
+		this.datosDeInteres = datosDeInteres.trim();
+	}
+	public void setMetros(String metros) {
+		this.metros = metros.trim();
+	}
+	public void setPrecio(String precio) {
+		this.precio = precio.trim();
+	}
+	public void setRegimen(String regimen) {
+		this.regimen = regimen.trim();
+	}
 }
+
