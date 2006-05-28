34c46e5bbffd8ebce31636f34f0b05493302e2c6
joseluis <joseluis>
2006-05-28 09:42:59 +0000

    *** empty log message ***

--- CVS-INMOBIS/Inmobis/src/com/inmobis/bajas/EliminarMensaje.java
+++ CVS-INMOBIS/Inmobis/src/com/inmobis/bajas/EliminarMensaje.java
@@ -3,21 +3,73 @@ package com.inmobis.bajas;
 import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 
+import com.inmobis.bbdd.CreadorGestores;
+import com.inmobis.bbdd.mensajes.GestorMensajesBD;
+import com.inmobis.bbdd.mensajes.MensajesBean;
 import com.inmobis.struts.action.BorraClienteAction;
+import com.inmobis.struts.form.MensajeForm;
 
 public class EliminarMensaje extends Eliminar{
 
 	private static final Logger log = Logger.getLogger(BorraClienteAction.class);
 	@Override
 	public boolean validarRegistrado(ActionForm id) {
-		// TODO Auto-generated method stub
-		return false;
+		// Metodo que mira su existe en la base de datos
+		MensajesBean msg = new MensajesBean();
+		msg.setIdMensaje(((MensajeForm)id).getIdMensaje());
+		msg.setOrigen(((MensajeForm)id).getOrigen());
+		msg.setDestino(((MensajeForm)id).getDestino());
+		msg.setFecha(((MensajeForm)id).getFecha());
+		//se crea un empleado de la base de datos
+		GestorMensajesBD gestMsg = (GestorMensajesBD) CreadorGestores.crearGestor("mensaje",msg);
+
+		//mensajes para ver los errores
+		if (log.isInfoEnabled()){
+			log.info("EliminarMensaje1: Antes de la BDD tengo idMensaje: "+ msg.getIdMensaje());
+		}
+		//miro a ver si el mensaje está en la BBDD
+		try {
+			gestMsg.BusquedaDetallada();
+		} catch (Exception E) {
+			if (log.isInfoEnabled()){
+				log.info("EliminarMensaje2: no he encontrado ese mensaje en la bbdd");
+			}
+			return false;
+		}
+		
+		if (log.isInfoEnabled()){
+			log.info("EliminarMensaje3: Mensaje encontrado  ");
+		}
+		return true;	
 	}
 
 	@Override
 	public boolean eliminarDesdeED(ActionForm id) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		MensajesBean msg = new MensajesBean();
+		msg.setIdMensaje(((MensajeForm)id).getIdMensaje());
+		msg.setOrigen(((MensajeForm)id).getOrigen());
+		msg.setDestino(((MensajeForm)id).getDestino());
+		msg.setFecha(((MensajeForm)id).getFecha());
+		//se crea un empleado de la base de datos
+		GestorMensajesBD gestMsg = (GestorMensajesBD) CreadorGestores.crearGestor("mensaje",msg);
 
+		//mensajes para ver los errores
+		if (log.isInfoEnabled()){
+			log.info("EliminarMensaje1: Antes de la BDD tengo idMensaje: "+ msg.getIdMensaje());
+		}
+		//Borramos el mensaje
+		try {
+			gestMsg.delete();
+		} catch (Exception E) {
+			if (log.isInfoEnabled()){
+				log.info("EliminarMensaje2: Fallo al borrar de la bbdd");
+			}
+			return false;
+		}
+		
+		if (log.isInfoEnabled()){
+			log.info("EliminarMensaje3: Mensaje Eliminado ");
+		}
+		return true;	
+	}
 }
