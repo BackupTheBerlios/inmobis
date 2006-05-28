b1d120bb7e75db8571608f432d126d46ce403fea
joseluis <joseluis>
2006-05-28 10:20:02 +0000

    *** empty log message ***

---
+++ CVS-INMOBIS/Inmobis/src/com/inmobis/altas/IntroducirMensaje.java
@@ -0,0 +1,39 @@
+package com.inmobis.altas;
+
+import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMessages;
+
+import com.inmobis.bbdd.CreadorGestores;
+import com.inmobis.bbdd.GeneradorDeCodigos;
+import com.inmobis.bbdd.mensajes.GestorMensajesBD;
+import com.inmobis.bbdd.mensajes.MensajesBean;
+import com.inmobis.struts.form.MensajeForm;
+
+public class IntroducirMensaje extends Introducir{
+	private static final Logger i_log = Logger.getLogger(IntroducirInmueble.class);
+	
+	@Override
+	public ActionMessages introduce(ActionForm form) {
+		MensajeForm msgForm = (MensajeForm) form;
+		GeneradorDeCodigos gc=GeneradorDeCodigos.getGeneradorDeCodigos();
+		ActionMessages errors= new ActionMessages();
+		//Creamos y rellenamos el objeto Bean para el inmueble
+		MensajesBean msgBean = new MensajesBean();
+		msgBean.setIdMensaje(gc.asignaCodigoMensaje());
+		msgBean.setOrigen(msgForm.getOrigen());
+		msgBean.setDestino(msgForm.getDestino());
+		msgBean.setFecha(msgForm.getFecha());
+		msgBean.setAsunto(msgForm.getAsunto());
+		msgBean.setLeido('N');
+		msgBean.setTexto(msgForm.getTexto());
+
+		if(i_log.isInfoEnabled())
+			i_log.info("Codigo Inmueble:" + msgBean.getIdMensaje());
+		
+		GestorMensajesBD gestMsg = (GestorMensajesBD) CreadorGestores.crearGestor("mensaje",msgBean);
+		
+		return errors;
+	}
+
+}
