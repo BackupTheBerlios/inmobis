a196392356acbe8da4a8b068d8c5031f8d859317
itzipg <itzipg>
2006-05-29 15:48:53 +0000

    *** empty log message ***

--- CVS-INMOBIS/Inmobis/src/com/inmobis/struts/form/TraspasarClientesForm.java
+++ CVS-INMOBIS/Inmobis/src/com/inmobis/struts/form/TraspasarClientesForm.java
@@ -7,35 +7,44 @@ import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
 
-public class TraspasarClientesPrevForm extends ActionForm {
+public class TraspasarClientesForm extends ActionForm {
+	
+	private static final Logger i_log = Logger.getLogger(TraspasarClientesForm.class);
 	private String idCliente;
-	private static final Logger i_log = Logger.getLogger(TraspasarClientesPrevForm.class);
+	private String idAgente;
 	public void reset(ActionMapping mapping, HttpServletRequest request) {
-		idCliente="";		
+		idCliente="";
+		idAgente="";
 	}
 	
 	public ActionErrors validate(
 		ActionMapping mapping,
 		HttpServletRequest request) {
+
 		ActionErrors errors=new ActionErrors();
 		
 		if (i_log.isInfoEnabled()){
-			i_log.info("Cliente: "+idCliente);
+			i_log.info("Cliente: "+idCliente +". Agente: "+idAgente);
 		}
-
+		
 		return errors;
 	}
-	
+
 	//Getter
 	public String getIdCliente(){
 		return this.idCliente;
 	}
-	
-	//Setter
-	public void setIdCliente(String id){
-		this.idCliente=id.trim();
+	public String getIdAgente(){
+		return this.idAgente;
 	}
 	
+	//Setter
+	public void setIdCliente(String idC){
+		this.idCliente=idC.trim();
+	}
+	public void setIdAgente(String idA){
+		this.idAgente=idA.trim();
+	}
 
 }
 
