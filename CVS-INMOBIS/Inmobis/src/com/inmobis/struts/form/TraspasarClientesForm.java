c71d9bc3ae12ee32a4a408221d961a6760abc95e
itzipg <itzipg>
2006-06-01 08:24:43 +0000

    *** empty log message ***

--- CVS-INMOBIS/Inmobis/src/com/inmobis/struts/form/TraspasarClientesForm.java
+++ CVS-INMOBIS/Inmobis/src/com/inmobis/struts/form/TraspasarClientesForm.java
@@ -6,26 +6,30 @@ import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
 
 public class TraspasarClientesForm extends ActionForm {
 	
 	private static final Logger i_log = Logger.getLogger(TraspasarClientesForm.class);
 	private String idCliente;
-	private String idAgente;
+	private String idEmpleado;
 	public void reset(ActionMapping mapping, HttpServletRequest request) {
+		idEmpleado="";
 		idCliente="";
-		idAgente="";
 	}
 	
 	public ActionErrors validate(
 		ActionMapping mapping,
 		HttpServletRequest request) {
 
-		ActionErrors errors=new ActionErrors();
-		
 		if (i_log.isInfoEnabled()){
-			i_log.info("Cliente: "+idCliente +". Agente: "+idAgente);
+			i_log.info("Cliente: "+idCliente +". Agente: "+idEmpleado);
 		}
+		ActionErrors errors=new ActionErrors();
+		if (idCliente.equals(""))
+			errors.add("traspasarCliente", new ActionMessage("No ha elegido un cliente"));
+		if ( idEmpleado.equals(""))
+			errors.add("traspasarCliente", new ActionMessage("No ha elegido un agente"));
 		
 		return errors;
 	}
@@ -34,16 +38,16 @@ public class TraspasarClientesForm extends ActionForm {
 	public String getIdCliente(){
 		return this.idCliente;
 	}
-	public String getIdAgente(){
-		return this.idAgente;
+	public String getIdEmpleado(){
+		return this.idEmpleado;
 	}
 	
 	//Setter
 	public void setIdCliente(String idC){
-		this.idCliente=idC.trim();
+		this.idCliente=idC;
 	}
-	public void setIdAgente(String idA){
-		this.idAgente=idA.trim();
+	public void setIdEmpleado(String idA){
+		this.idEmpleado=idA;
 	}
 
 }
